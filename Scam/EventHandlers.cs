858aed56344c1d832308882f0c7eb6feb0327f38
Thunder <thundermaker300@gmail.com>
2022-12-30 20:57:33 -0500

    Create

---
+++ Scam/EventHandlers.cs
@@ -0,0 +1,46 @@
+﻿using Exiled.API.Enums;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles.PlayableScps.Scp106;
+using UnityEngine;
+using MEC;
+
+namespace Scam
+{
+    public class EventHandlers
+    {
+        private readonly MainPlugin Plugin;
+
+        public EventHandlers(MainPlugin plugin) => Plugin = plugin;
+
+        public void OnInteractingDoor(InteractingDoorEventArgs ev)
+        {
+            if (ev.Door.IsKeycardDoor && ev.IsAllowed && Random.value > .5)
+            {
+                ev.IsAllowed = false;
+                ev.Door.PlaySound(DoorBeepType.PermissionDenied);
+            }
+        }
+
+        public void OnFailingEscapePocketDimension(FailingEscapePocketDimensionEventArgs ev)
+        {
+            ev.IsAllowed = false;
+            Vector3 exit = Scp106PocketExitFinder.GetBestExitPosition(ev.Player.Role.Base as PlayerRoles.FirstPersonControl.IFpcRole);
+            ev.Player.Teleport(exit);
+            Timing.CallDelayed(5f, () =>
+            {
+                ev.Player.Kill(DamageType.PocketDimension);
+            });
+        }
+
+        public void OnFailingEscapePocketDimension(FailingEscapePocketDimensionEventArgs ev)
+        {
+            ev.IsAllowed = false;
+            Vector3 exit = Scp106PocketExitFinder.GetBestExitPosition(ev.Player.Role.Base as PlayerRoles.FirstPersonControl.IFpcRole);
+            ev.Player.Teleport(exit);
+            Timing.CallDelayed(5f, () =>
+            {
+                ev.Player.Kill(DamageType.PocketDimension);
+            });
+        }
+    }
+}
---
+++ Scam/MainPlugin.cs
@@ -0,0 +1,42 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PlayerHandler = Exiled.Events.Handlers.Player;
+
+namespace Scam
+{
+    public class MainPlugin : Plugin<Config>
+    {
+        private EventHandlers Handlers;
+
+        public override void OnEnabled()
+        {
+            Handlers = new(this);
+
+            PlayerHandler.InteractingDoor += Handlers.OnInteractingDoor;
+            PlayerHandler.FailingEscapePocketDimension += Handlers.OnFailingEscapePocketDimension;
+
+            base.OnDisabled();
+        }
+
+        public override void OnDisabled()
+        {
+            PlayerHandler.InteractingDoor += Handlers.OnInteractingDoor;
+            PlayerHandler.FailingEscapePocketDimension += Handlers.OnFailingEscapePocketDimension;
+
+            Handlers = null;
+            base.OnDisabled();
+        }
+    }
+
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; }
+    }
+}
